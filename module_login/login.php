491c2ebdb1afc44069fddabf3fdcf7dfe03189ef
maryuri04 <https://github.com/maryuri04/test_practice.git>
2025-03-19 20:17:56 -0500

    se añadio repositorio del proyecto-02

---
+++ module_login/login.php
@@ -0,0 +1,78 @@
+// use Firebase\JWT\JWT;
+
+$payload = [
+    'user_id' => 123,
+    'email' => 'usuario@example.com',
+    'exp' => time() + 3600, // Expira en 1 hora
+];
+
+$secretkey = 'TuClaveSecretaSuperSegura';
+$token = JWT::encode($payload, $secretkey, 'HS256'); // para tokens con JWT 
+
+<?php
+session_start();
+require '../module_login/conexion.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    
+    $correo = trim($_POST['correo']);
+    $contraseña = trim($_POST['contrasenia']);
+    $nombres = trim($_POST['nombres']);
+    $apellidos = trim($_POST['apellidos']);
+    $tiporol = trim($_POST['tipo_rol']);
+
+    if (empty($correo) || empty($contraseña)) {
+        $_SESSION['error'] = "Por favor, completa todos los campos.";
+        header("Location: loggin.php");
+        exit;
+    }
+
+    try {
+        $stmt = $connect->prepare("SELECT id_funcionario, correo, contrasenia, nombres, apellidos, tipo_rol FROM funcionarios WHERE correo = :correo");
+        $stmt->bindParam(':correo', $correo);
+        $stmt->execute();
+
+        if ($stmt->rowCount() == 1) {
+            $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            if (password_verify($contraseña,  $user['contrasenia'])) {
+
+                $token = bin2hex(random_bytes(64));
+                // firma del token con HMAC
+                $secretkey = $contraseña;
+                $signature = hash_hmac('sha256', $token, $secretkey);
+                $signedToken = $token . '.' . $signature;
+                // encriptacion del token (cifrado simetrico con AES)
+                $iv = random_bytes(32); // vector de inicializacion (iv)
+                $encryptedToken = openssl_encrypt($token, 'AES-256-CBC', $secretkey, 0, $iv);
+
+    
+                $_SESSION['loggedin'] = true;
+                $_SESSION['id_funcionario'] = $user['id_funcionario'];
+                $_SESSION['correo'] = $user['correo'];
+                $_SESSION['nombres'] = $user['nombres'];
+                $_SESSION['apellidos'] = $user['apellidos'];
+                $_SESSION['tipo_rol'] = $user['tipo_rol'];
+                echo "Autenticación exitosa. Redirigiendo al dashboard...";
+                header("Location: ../dashboard.php");
+                exit;
+            } else {
+                $_SESSION['error'] = "Contraseña incorrecta.";
+                echo "Error: " . $_SESSION['error'];
+                header("Location: ../loggin.php");
+                exit;
+            }
+        } else {
+            $_SESSION['error'] = "Usuario no encontrado.";
+            echo "Error: " . $_SESSION['error'];
+            header("Location: ../loggin.php");
+            exit;
+        }
+    } catch (PDOException $e) {
+        $_SESSION['error'] = "Error: " . $e->getMessage();
+        echo "Error en la consulta: " . $_SESSION['error'];
+        header("Location: ../loggin.php");
+        exit;
+    }
+}
+error_log(print_r($_POST, true));
---
+++ module_login/new_user/new_user.php
@@ -0,0 +1,52 @@
+<?php
+require 'conexion.php';
+$correo = $_POST['correo']; // Correo proporcionado por el usuario
+$contraseña_plana = $_POST['contraseña']; // Contraseña proporcionada por el usuario
+$hashed_password = password_hash($contraseña_plana, PASSWORD_DEFAULT);
+
+try {
+    $stmt = $connect->prepare("INSERT INTO funcionarios (correo, contraseña) VALUES (:correo, :contraseña)");
+    $stmt->bindParam(':correo', $correo);
+    $stmt->bindParam(':contraseña', $hashed_password);
+    $stmt->execute();
+
+    echo "Usuario registrado correctamente.";
+} catch (PDOException $e) {
+    die("Error al registrar el usuario: " . $e->getMessage());
+}
+
+$correo = $_POST['correo'];
+$contraseña_plana = $_POST['contraseña'];
+
+try {
+    // Obtener el hash almacenado en la base de datos
+    $stmt = $connect->prepare("SELECT id_funcionario, correo, contraseña FROM funcionarios WHERE correo = :correo");
+    $stmt->bindParam(':correo', $correo);
+    $stmt->execute();
+
+    if ($stmt->rowCount() == 1) {
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        // Verificar la contraseña
+        if (password_verify($contraseña_plana, $user['contraseña'])) {
+            // Contraseña válida
+            $_SESSION['loggedin'] = true;
+            $_SESSION['id_funcionario'] = $user['id_funcionario'];
+            $_SESSION['correo'] = $user['correo'];
+            header("Location: dashboard.php");
+            exit;
+        } else {
+            // Contraseña incorrecta
+            $_SESSION['error'] = "Contraseña incorrecta.";
+            header("Location: loggin.html");
+            exit;
+        }
+    } else {
+        // Usuario no encontrado
+        $_SESSION['error'] = "Usuario no encontrado.";
+        header("Location: loggin.html");
+        exit;
+    }
+} catch (PDOException $e) {
+    die("Error en la consulta: " . $e->getMessage());
+}
